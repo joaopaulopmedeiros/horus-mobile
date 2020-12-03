005ff92ab923a35c14e0d24b46b6ced76b92352a
João Paulo Pereira de Medeiros <medeiros.p@academico.ifrn.edu.br>
2020-12-03 10:11:36 -0300

    Starting project - expo scaffolding

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>SUSI Rocks!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +17,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
