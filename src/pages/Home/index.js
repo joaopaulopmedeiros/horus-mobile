a2fac69b9502b01e30cfd698b0c6c4574c85e211
João Paulo Pereira de Medeiros <medeiros.p@academico.ifrn.edu.br>
2020-12-03 15:15:50 -0300

    Map component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,23 +1,31 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import MapView from "react-native-maps";
+import { customMapStyle } from "../../styles/maps/index";
 
 const Home = () => {
   const insets = useSafeAreaInsets();
 
   return (
-    <View
+    <MapView
+      tooltip={true}
+      initialRegion={{
+        latitude: -5.8453006,
+        longitude: -35.2697694,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
       style={{
         paddingTop: insets.top,
         paddingBottom: insets.bottom,
 
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        justifyContent: "center",
+        alignItems: "center",
       }}
-    >
-      <Text>SUSI</Text>
-    </View>
+      customMapStyle={customMapStyle}
+    ></MapView>
   );
 };
 
