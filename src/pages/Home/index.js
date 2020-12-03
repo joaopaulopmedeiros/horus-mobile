e6626d78786129b2ec77f1aee818408b4a0046e2
João Paulo Pereira de Medeiros <medeiros.p@academico.ifrn.edu.br>
2020-12-03 15:22:11 -0300

    crossHair component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,31 +1,50 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import MapView from "react-native-maps";
 import { customMapStyle } from "../../styles/maps/index";
+import crossHair from "../../../assets/crosshair.png";
 
 const Home = () => {
   const insets = useSafeAreaInsets();
 
   return (
-    <MapView
-      tooltip={true}
-      initialRegion={{
-        latitude: -5.8453006,
-        longitude: -35.2697694,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-      style={{
-        paddingTop: insets.top,
-        paddingBottom: insets.bottom,
+    <>
+      <MapView
+        tooltip={true}
+        initialRegion={{
+          latitude: -5.8453006,
+          longitude: -35.2697694,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        style={{
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom,
 
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-      customMapStyle={customMapStyle}
-    ></MapView>
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        customMapStyle={customMapStyle}
+      ></MapView>
+      <View
+        style={{
+          position: "absolute",
+          top: insets.top + 40,
+          right: 40,
+          width: 40,
+          height: 40,
+          borderRadius: 16,
+          backgroundColor: "#FFFFFF",
+          zIndex: 5,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Image source={crossHair} />
+      </View>
+    </>
   );
 };
 
