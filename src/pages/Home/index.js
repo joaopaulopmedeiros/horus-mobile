53d587a22ae5c60a103bfeeaa00cdc38da6b1cce
João Paulo Pereira de Medeiros <medeiros.p@academico.ifrn.edu.br>
2020-12-08 17:01:01 -0300

    New default location

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -22,31 +22,32 @@ const Home = () => {
   const [currentRegion, setCurrentRegion] = useState(null);
 
   useEffect(() => {
-    async function loadInitialPosition() {
-      try {
-        const { granted } = await requestPermissionsAsync();
-        if (granted) {
-          const { coords } = await getCurrentPositionAsync({
-            enableHighAccuracy: true,
-          });
-
-          const { latitude, longitude } = coords;
-
-          setCurrentRegion({
-            latitude,
-            longitude,
-            latitudeDelta: 0.04,
-            longitudeDelta: 0.04,
-          });
-        }
-      } catch (error) {
-        console.log(error);
-
-        setCurrentRegion(defaultMapLocation);
-      }
-    }
     loadInitialPosition();
   }, []);
+
+  async function loadInitialPosition() {
+    try {
+      const { granted } = await requestPermissionsAsync();
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true,
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+
+      setCurrentRegion(defaultMapLocation);
+    }
+  }
 
   function handleRegionChanged(region) {
     setCurrentRegion(region);
--- src/utils/defaultMapLocation.js
+++ src/utils/defaultMapLocation.js
@@ -1,6 +1,6 @@
 export default defaultMapLocation = {
-  latitude: -5.8453006,
-  longitude: -35.2697694,
-  latitudeDelta: 0.0922,
+  latitude: -5.7490999,
+  longitude: -35.262599,
+  latitudeDelta: 0.2922,
   longitudeDelta: 0.0421,
 };
