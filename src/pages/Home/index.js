e883aaac0c76b859928c65bb9d424522a6db0c44
João Paulo Pereira de Medeiros <medeiros.p@academico.ifrn.edu.br>
2020-12-08 15:28:34 -0300

    feat: styled-components

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { View, Image } from "react-native";
+
+import { Image } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import MapView, { Marker } from "react-native-maps";
+
+import { GPS } from "./styles";
 import { customMapStyle } from "../../styles/maps/index";
-import crossHair from "../../../assets/crosshair.png";
+
 import marker from "../../../assets/marker.png";
+import crossHair from "../../../assets/crosshair.png";
 
 const Home = () => {
   const insets = useSafeAreaInsets();
@@ -19,15 +23,12 @@ const Home = () => {
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
+        customMapStyle={customMapStyle}
         style={{
-          paddingTop: insets.top,
-          paddingBottom: insets.bottom,
-
           flex: 1,
           justifyContent: "center",
           alignItems: "center",
         }}
-        customMapStyle={customMapStyle}
       >
         <Marker
           coordinate={{
@@ -38,22 +39,9 @@ const Home = () => {
           <Image source={marker} />
         </Marker>
       </MapView>
-      <View
-        style={{
-          position: "absolute",
-          top: insets.top + 40,
-          right: 40,
-          width: 40,
-          height: 40,
-          borderRadius: 16,
-          backgroundColor: "#FFFFFF",
-          zIndex: 5,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+      <GPS marginTop={insets.top + 8}>
         <Image source={crossHair} />
-      </View>
+      </GPS>
     </>
   );
 };
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const GPS = styled.View`
+  position: absolute;
+  top: ${(props) => props.marginTop}px;
+  right: 40px;
+  width: 40px;
+  height: 40px;
+  border-radius: 16px;
+  background-color: #ffffff;
+  z-index: 5;
+  justify-content: center;
+  align-items: center;
+`;
