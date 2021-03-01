9da3046b0cf98d9e1d1326c584e0e8e776f17be0
João Paulo M <joaopaulopmedeiros@gmail.com>
2021-03-01 12:26:07 -0300

    style: last cvli card component

--- src/pages/Cvli/index.js
+++ src/pages/Cvli/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Header from "../../components/Header";
 import { Container } from "../../components/Container";
+import ListLast from "./ListLast";
 
 const Cvli = () => {
   const insets = useSafeAreaInsets();
@@ -9,6 +10,7 @@ const Cvli = () => {
   return (
     <Container marginTop={insets.top}>
       <Header title="Crimes Registrados" />
+      <ListLast />
     </Container>
   );
 };
