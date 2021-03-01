c707e11345ef3c9ffc19dc3e31b83bf26dc0b5a6
João Paulo M <joaopaulopmedeiros@gmail.com>
2021-03-01 12:27:27 -0300

    style: last cvli card

--- src/pages/Cvli/ListLast.js
+++ src/pages/Cvli/ListLast.js
@@ -129,6 +129,100 @@ const ListLast = () => {
               </Text>
             </View>
           </TouchableOpacity>
+          <TouchableOpacity
+            activeOpacity={1}
+            style={{
+              backgroundColor: "#fff",
+              borderWidth: 2,
+              borderColor: "#eee",
+              height: 125,
+              width: 280,
+              borderRadius: 8,
+              paddingHorizontal: 16,
+              paddingTop: 20,
+              paddingBottom: 16,
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: "Montserrat_600SemiBold",
+                fontSize: 16,
+                width: "90%",
+                height: 24,
+                color: "rgba(20,119,248,0.8)",
+              }}
+            >
+              Fiat Uno Roubado
+            </Text>
+            <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+              <Text
+                style={{
+                  fontFamily: "Montserrat_500Medium",
+                  fontSize: 10,
+                  width: "62%",
+                  height: 46,
+                  color: "#77838F",
+                }}
+              >
+                Lorem Ipsum is simply text of the printing and typesetting
+                industry.
+              </Text>
+              <View>
+                <TouchableOpacity
+                  style={{
+                    marginBottom: 2,
+                  }}
+                >
+                  <Text
+                    style={{
+                      fontFamily: "Montserrat_500Medium",
+                      fontSize: 10,
+                      height: 24,
+                      color: "#fff",
+                      backgroundColor: "rgba(20,119,248,0.8)",
+                      paddingVertical: 3,
+                      paddingHorizontal: 6,
+                      borderRadius: 3,
+                    }}
+                  >
+                    Ver Registro
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={{
+                    borderColor: "#000",
+                  }}
+                >
+                  <Text
+                    style={{
+                      fontFamily: "Montserrat_500Medium",
+                      fontSize: 10,
+                      height: 22,
+                      color: "#000",
+                      paddingVertical: 3,
+                      paddingHorizontal: 6,
+                      borderWidth: 1,
+                      borderRadius: 3,
+                    }}
+                  >
+                    Denunciar
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={{marginTop: 4, flexDirection: 'row', alignItems: 'center'}}>
+              <Image source={require("../../../assets/verified.png")} />
+              <Text
+                style={{
+                  fontFamily: "Montserrat_500Medium",
+                  fontSize: 10,
+                  marginLeft: 4
+                }}
+              >
+                Verificado
+              </Text>
+            </View>
+          </TouchableOpacity>
         </ScrollView>
       </View>
     </View>
