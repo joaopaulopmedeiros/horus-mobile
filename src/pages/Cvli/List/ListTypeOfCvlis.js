428413bf792490507877c49f064641f0f1e7c0e9
João Paulo <jpaulope@indracompany.com>
2021-03-20 20:59:54 -0300

    refactor: list type of cvlis

--- src/pages/Cvli/List/ListTypeOfCvlis.js
+++ src/pages/Cvli/List/ListTypeOfCvlis.js
@@ -1,220 +1,79 @@
 import React from "react";
-import { View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
+import { View, Text, ScrollView, TouchableOpacity, Image, StyleSheet } from "react-native";
 
 const ListTypeOfCvlis = () => {
+  const typeOfCvlis = [
+    { id: 1, title: 'Roubo de Veículos', imgPath: require("../../../../assets/roubo-veiculos.png") },
+    { id: 2, title: 'Furtos', imgPath: require("../../../../assets/furto.png") },
+    { id: 3, title: 'Roubo à mão armada', imgPath: require("../../../../assets/roubo-mao-armada.png") },
+    { id: 4, title: 'Agressão', imgPath: require("../../../../assets/agressao.png") },
+    { id: 5, title: 'Acidente de trânsito', imgPath: require("../../../../assets/acidente-transito.png") },
+    { id: 6, title: 'Maus tratos a animais', imgPath: require("../../../../assets/maustratos-animais.png") },
+  ];
   return (
-    <View
-      style={{
-        width: "100%",
-        height: "30%",
-        height: 540,
-        paddingLeft: "8%",
-        paddingTop: "2%",
-        //backgroundColor: 'green'
-      }}
-    >
-      <Text
-        style={{
-          textTransform: "uppercase",
-          color: "rgba(20,119,248,0.8)",
-          fontFamily: "Montserrat_700Bold",
-        }}
-      >
+    <View style={styles.wrapper}>
+      <Text style={styles.title}>
         Tipos de Crimes
       </Text>
-      <View
-        style={{
-          flexDirection: "row",
-          flexWrap: 'wrap',
-          marginTop: 16,
-          marginBottom: 32,
-          //backgroundColor: 'green',
-        }}
-      >
-        <TouchableOpacity
-          activeOpacity={1}
-          style={{
-            backgroundColor: "#fff",
-            height: 125,
-            width: '40%',
-            maxWidth: 160,
-            borderRadius: 8,
-            paddingHorizontal: 16,
-            paddingTop: 20,
-            paddingBottom: 16,
-            justifyContent: "center",
-            alignItems: "center",            
-            marginVertical: 8,
-            marginRight: 16
-          }}
-        >
-          <Image source={require("../../../../assets/roubo-veiculos.png")} />
-          <Text
-            style={{
-              fontFamily: "Montserrat_600SemiBold",
-              fontSize: 13,
-              width: "90%",
-              color: "rgba(20,119,248,0.8)",
-              textAlign: "center",
-              marginTop: 2
-            }}
+      <View style={styles.cvlisWrapper}>
+        {typeOfCvlis.map(item => (
+          <TouchableOpacity
+            key={item.id} 
+            activeOpacity={1}
+            style={styles.card}
           >
-            Roubo de Veículos
+            <Image source={item.imgPath}/>
+            <Text style={styles.cardTitle}>
+              {item.title}
           </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          activeOpacity={1}
-          style={{
-            backgroundColor: "#fff",
-            height: 125,
-            width: '40%',
-            maxWidth: 160,
-            borderRadius: 8,
-            paddingHorizontal: 16,
-            paddingTop: 20,
-            paddingBottom: 16,
-            justifyContent: "center",
-            alignItems: "center",
-            marginVertical: 8,
-            marginHorizontal: 16
-          }}
-        >
-          <Image source={require("../../../../assets/furto.png")} />
-          <Text
-            style={{
-              fontFamily: "Montserrat_600SemiBold",
-              fontSize: 13,
-              width: "90%",
-              color: "rgba(20,119,248,0.8)",
-              textAlign: "center",
-            }}
-          >
-            Furtos
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          activeOpacity={1}
-          style={{
-            backgroundColor: "#fff",
-            height: 125,
-            width: '40%',
-            maxWidth: 160,
-            borderRadius: 8,
-            paddingHorizontal: 16,
-            paddingTop: 20,
-            paddingBottom: 16,
-            justifyContent: "center",
-            alignItems: "center",
-            marginVertical: 8,
-            marginRight: 16
-          }}
-        >
-          <Image source={require("../../../../assets/roubo-mao-armada.png")} />
-          <Text
-            style={{
-              fontFamily: "Montserrat_600SemiBold",
-              fontSize: 13,
-              width: "90%",
-              color: "rgba(20,119,248,0.8)",
-              textAlign: "center",
-            }}
-          >
-            Roubo à mão armada
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          activeOpacity={1}
-          style={{
-            backgroundColor: "#fff",
-            height: 125,
-            width: '40%',
-            maxWidth: 160,
-            borderRadius: 8,
-            paddingHorizontal: 16,
-            paddingTop: 20,
-            paddingBottom: 16,
-            justifyContent: "center",
-            alignItems: "center",
-            marginVertical: 8,
-            marginHorizontal: 16
-          }}
-        >
-          <Image source={require("../../../../assets/agressao.png")} />
-          <Text
-            style={{
-              fontFamily: "Montserrat_600SemiBold",
-              fontSize: 13,
-              width: "90%",
-              color: "rgba(20,119,248,0.8)",
-              textAlign: "center",
-            }}
-          >
-            Agressão
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          activeOpacity={1}
-          style={{
-            backgroundColor: "#fff",
-            height: 125,
-            width: '40%',
-            maxWidth: 160,
-            borderRadius: 8,
-            paddingHorizontal: 16,
-            paddingTop: 20,
-            paddingBottom: 16,
-            justifyContent: "center",
-            alignItems: "center",
-            marginVertical: 8,
-            marginRight: 16
-          }}
-        >
-          <Image source={require("../../../../assets/acidente-transito.png")} />
-          <Text
-            style={{
-              fontFamily: "Montserrat_600SemiBold",
-              fontSize: 13,
-              width: "90%",
-              color: "rgba(20,119,248,0.8)",
-              textAlign: "center",
-            }}
-          >
-            Acidente de trânsito
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          activeOpacity={1}
-          style={{
-            backgroundColor: "#fff",
-            height: 125,
-            width: '40%',
-            maxWidth: 160,
-            borderRadius: 8,
-            paddingHorizontal: 16,
-            paddingTop: 20,
-            paddingBottom: 16,
-            justifyContent: "center",
-            alignItems: "center",
-            marginVertical: 8,
-            marginHorizontal: 16
-          }}
-        >
-          <Image source={require("../../../../assets/maltratos-animais.png")} />
-          <Text
-            style={{
-              fontFamily: "Montserrat_600SemiBold",
-              fontSize: 13,
-              width: "90%",
-              color: "rgba(20,119,248,0.8)",
-              textAlign: "center",
-            }}
-          >
-            Maltratos a animais
-          </Text>
-        </TouchableOpacity>
+          </TouchableOpacity>
+        ))}
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  wrapper: {
+    width: "100%",
+    height: "30%",
+    height: 540,
+    paddingLeft: "8%",
+    paddingTop: "2%",
+  },
+  title: {
+    textTransform: "uppercase",
+    color: "rgba(20,119,248,0.8)",
+    fontFamily: "Montserrat_700Bold",
+  },
+  cvlisWrapper: {
+    flexDirection: "row",
+    flexWrap: 'wrap',
+    marginTop: 16,
+    marginBottom: 32,
+  },
+  card: {
+    backgroundColor: "#fff",
+    height: 125,
+    width: '40%',
+    maxWidth: 160,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    paddingTop: 20,
+    paddingBottom: 16,
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 8,
+    marginRight: 16
+  },
+  cardTitle: {
+    fontFamily: "Montserrat_600SemiBold",
+    fontSize: 13,
+    width: "90%",
+    color: "rgba(20,119,248,0.8)",
+    textAlign: "center",
+    marginTop: 2
+  }
+})
 
 export default ListTypeOfCvlis;
