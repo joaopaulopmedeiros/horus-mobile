84043d1950eb760f54b013f5b9c13ceded05b211
João Paulo <jpaulope@indracompany.com>
2021-03-21 15:52:21 -0300

    feat: check if user is logged in and restrict cvli registration

--- src/pages/Cvli/Register/index.js
+++ src/pages/Cvli/Register/index.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState, useContext, useEffect, useCallback } from "react";
+import { Alert, Modal, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Header from "../../../components/Header";
 import { Container } from "../../../components/Container";
+import { AuthContext } from "../../../contexts/auth";
+import { useFocusEffect } from '@react-navigation/native';
 
-const CvliRegister = () => {
+const CvliRegister = ({ navigation }) => {
   const insets = useSafeAreaInsets();
+
+  const { loggedIn } = useContext(AuthContext);
+
+  function handleNavigationToAccountLogin() {
+    navigation.navigate('Conta', { screen: 'Login', initial: false });
+  }
+
+  function emitAlert() {
+    Alert.alert(
+      "Aviso",
+      "Para cadastrar um crime, faça login.",
+      [
+        { text: "OK", onPress: () => handleNavigationToAccountLogin() }
+      ],
+      { cancelable: false }
+    );
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      if (!loggedIn) emitAlert();
+    }, [loggedIn])
+  );
 
   return (
     <Container marginTop={insets.top}>
@@ -12,5 +38,14 @@ const CvliRegister = () => {
     </Container>
   );
 };
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%'
+  },
+})
 
 export default CvliRegister;
--- src/pages/Cvli/index.js
+++ src/pages/Cvli/index.js
@@ -3,9 +3,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 import CvliMain from "./Main";
 import CvliDetails from "./Details";
 import CvliListByType from "./ListByType";
+import CvliRegister from "./Register";
 
 const Stack = createStackNavigator();
-
 
 const Cvli = () => {
     return (
@@ -19,6 +19,7 @@ const Cvli = () => {
             <Stack.Screen name="Main" component={CvliMain} />
             <Stack.Screen name="ListaPorTipo" component={CvliListByType} />
             <Stack.Screen name="Detalhes" component={CvliDetails} />
+            <Stack.Screen name="Cadastrar" component={CvliRegister}/>
         </Stack.Navigator>
     );
 }
